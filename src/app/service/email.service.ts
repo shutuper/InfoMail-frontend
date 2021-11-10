d108288e207d0eddbeccc7f9028e9d7f4cb33e6d
Lastzlo <z.last.zlo@gmail.com>
2021-11-10 17:16:31 +0200

    feat add HttpHeaders

--- src/app/service/email.service.ts
+++ src/app/service/email.service.ts
@@ -1,17 +1,26 @@
 import {Injectable} from "@angular/core";
 import {environment} from "../../environments/environment";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Email} from "../model/email";
 
 @Injectable({providedIn: 'root'})
 export class EmailService {
   private apiServerUrl = environment.apiBaseUrl;
+  public authorizationToken = environment.authorizationToken;
 
-  constructor(private http: HttpClient){}
+  constructor(
+    private http: HttpClient,
+  ){}
 
   public sendEmail(email: Email): void {
+    const headers = new HttpHeaders().append('Authorization', this.authorizationToken);
+
     this.http
-      .post<Email>(`${this.apiServerUrl}/api/v1/emails/`, email)
+      .post<Email>(
+        `${this.apiServerUrl}/api/v1/emails/`,
+        email,
+        {headers: headers}
+      )
       .subscribe((response: Email) => console.log(response));
   }
 }
