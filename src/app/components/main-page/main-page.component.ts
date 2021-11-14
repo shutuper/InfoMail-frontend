da7360ab46ab0a30dc3bad49db22a5ee9bcb3b0f
Lastzlo <z.last.zlo@gmail.com>
2021-11-15 01:43:08 +0200

    feat(onLogout) use AuthenticationService

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../../service/authentication.service";
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +8,12 @@ import {Component, OnInit} from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
 
+  onLogout() {
+    this.authService.logout();
+  }
 }
