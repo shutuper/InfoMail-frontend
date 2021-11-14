382c1ec8c73f9cffaafce205c535a5dec2af2d2e
Lastzlo <z.last.zlo@gmail.com>
2021-11-14 22:49:48 +0200

    feat use AuthService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./service/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'infomail-frontend';
+
+  constructor(public authService: AuthService) { }
+
+  ngOnInit(): void {
+    if(! this.authService.hasAuthToken()) {
+      console.log("Not Authorization user")
+    }
+  }
 }
