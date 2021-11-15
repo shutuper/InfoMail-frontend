195cf0a3fd85cfafcbf3766c4d194d6661afe7f7
Lastzlo <z.last.zlo@gmail.com>
2021-11-15 16:01:45 +0200

    feat(tryToAuthenticate, logout) use UserService, set up/off userEmail

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -2,13 +2,14 @@ import {Injectable} from "@angular/core";
 import {HttpClient, HttpResponse} from "@angular/common/http";
 import {User} from "../model/user";
 import {environment} from "../../environments/environment";
+import {UserService} from "./user.service";
 
 @Injectable({providedIn: 'root'})
 export class AuthenticationService {
   private apiServerUrl = environment.apiBaseUrl
 
   constructor(
-    private http: HttpClient,
+    private http: HttpClient, private userService: UserService
   ){}
 
   public hasAuthToken(): boolean {
@@ -39,6 +40,8 @@ export class AuthenticationService {
         error: (e) => console.error("Credentials Bad"),
         complete: () => {
           console.log("Auth success, token added to localStorage")
+          console.log("Setup user email in userService")
+          this.userService.userEmail = userCredentials.email
         }
       });
   }
@@ -46,5 +49,6 @@ export class AuthenticationService {
   logout(): void {
     localStorage.removeItem("token");
     console.log("Token removed from localStorage")
+    this.userService.userEmail = null;
   }
 }
