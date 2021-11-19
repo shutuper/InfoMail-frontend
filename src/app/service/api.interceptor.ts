d4aef3e85b4fb347b02bf69d487cf211d27e9eba
Lastzlo <z.last.zlo@gmail.com>
2021-11-19 18:26:45 +0200

    refactor(intercept) add check HttpErrorResponse

--- src/app/service/api.interceptor.ts
+++ src/app/service/api.interceptor.ts
@@ -1,18 +1,42 @@
 import {Injectable} from "@angular/core";
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest,} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest,} from "@angular/common/http";
+import {Observable, tap} from "rxjs";
 import {AuthenticationService} from "./authentication.service";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthenticationService) {}
+  constructor(private authService: AuthenticationService, private router: Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     //put Authorization Token
     const authReq = req.clone({
       headers: req.headers.set('Authorization', this.authService.getAuthToken()),
     });
+    return next.handle(authReq).pipe(
+      tap({
+        error: (err: HttpErrorResponse) => {
+          if(err.status === 403) this.logout();
+          if(err.status === 503) this.openErrorPage("Can't connect to server");
+        }
+      })
+    );
+  }
 
-    return next.handle(authReq);
+  private logout(): void {
+    this.authService.logout();
+    this.openWelcomePage();
+  }
+
+  private openWelcomePage() {
+    console.log("Navigate to welcome page");
+    this.router.navigate(['welcome']);
+  }
+
+  private openErrorPage(message: string) {
+    console.error(message)
+
+    console.log("Navigate to Error page");
+    this.router.navigate(['error']);
   }
 }
