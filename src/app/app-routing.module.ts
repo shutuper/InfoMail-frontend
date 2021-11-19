014b4ecc6134317093bc8d1143889222e8ba5028
Lastzlo <z.last.zlo@gmail.com>
2021-11-19 14:20:09 +0200

    feat add 'welcome' and 'home' Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {WelcomePageComponent} from "./components/welcome-page/welcome-page.component";
+import {HomePageComponent} from "./components/home-page/home-page.component";
 
 const routes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'home', component: HomePageComponent }
 ];
 
 @NgModule({
