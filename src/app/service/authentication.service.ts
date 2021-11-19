3371260979bc8408b23c63e45cfec6bed20a6b3d
Lastzlo <z.last.zlo@gmail.com>
2021-11-19 16:12:30 +0200

    feat add methods sendTestRequest, checkIsAuthTokenActual

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpResponse} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http";
 import {User} from "../model/user";
 import {Observable} from "rxjs";
 import {Router} from "@angular/router";
@@ -12,7 +12,26 @@ export class AuthenticationService {
   ){}
 
   public hasAuthToken(): boolean {
-    return localStorage.getItem("token") !== null;
+    const tokenAtStorage = localStorage.getItem("token");
+    return tokenAtStorage !== null && tokenAtStorage !== '';
+  }
+
+  public checkIsAuthTokenActual(): void {
+    if(! this.hasAuthToken()) {
+      console.log("Navigate to welcome page");
+      this.router.navigate(['welcome']);
+      return;
+    }
+
+    this.sendTestRequest().subscribe({
+      error: (err: HttpErrorResponse) => {
+        if(err.status == 504) console.log("Can't connect to server", err);
+        if(err.status == 403) {
+          console.log("Can't authenticate", err);
+          this.logout();
+        }
+      }
+    });
   }
 
   private setAuthToken(token: string): void {
@@ -38,6 +57,10 @@ export class AuthenticationService {
 
   private sendUserCredentials(userCredentials: User): Observable<HttpResponse<any>> {
     return this.http.post(`/api/v1/authenticate`, userCredentials, {observe: 'response'});
+  }
+
+  public sendTestRequest(): Observable<any> {
+    return this.http.get('api/v1/registration/sayHi', {observe: 'response'});
   }
 
   public logout(): void {
