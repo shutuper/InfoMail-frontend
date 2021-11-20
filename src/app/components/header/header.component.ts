db3d1f4f5ab3588f80bd56e66669a140305501f0
Lastzlo <z.last.zlo@gmail.com>
2021-11-20 23:33:09 +0200

    feat add methods: setUserEmail, isUserAuthenticate, onLogout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from "../../service/authentication.service";
+import {UserService} from "../../service/user.service";
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-header',
@@ -8,14 +10,26 @@ import {AuthenticationService} from "../../service/authentication.service";
 })
 export class HeaderComponent implements OnInit {
 
+  userEmail: string = '';
 
-  constructor(private authService: AuthenticationService) { }
+  constructor(private authService: AuthenticationService, public userService: UserService) { }
 
   ngOnInit(): void {
+    if(this.isUserAuthenticate()) this.setUserEmail();
+  }
+
+  setUserEmail() {
+    this.userService.getUsersEmail().subscribe({
+      next: (user: User) => this.userEmail = user.email
+    });
   }
 
   isUserAuthenticate(): boolean {
     return this.authService.hasAuthToken();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 
 }
