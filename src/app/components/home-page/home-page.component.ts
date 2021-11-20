bc164577e897b4dfe10017ddfd8d432a1748863d
Lastzlo <z.last.zlo@gmail.com>
2021-11-20 23:44:10 +0200

    refactor remove old header and userEmail flow

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,8 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthenticationService} from "../../service/authentication.service";
-import {UserService} from "../../service/user.service";
-import {User} from "../../model/user";
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home-page',
@@ -11,27 +7,9 @@ import {Router} from "@angular/router";
 })
 export class HomePageComponent implements OnInit {
 
-  userEmail: string = '';
-
-  constructor(
-    private authService: AuthenticationService,
-    public userService: UserService,
-    private router: Router
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getUserEmail();
-  }
-
-  getUserEmail(): void {
-    this.userService.getUsersEmail().subscribe({
-      next: (user: User) => this.userEmail = user.email
-    });
-  }
-
-  onLogout() {
-    this.authService.logout();
-    this.router.navigate(['welcome']);
   }
 
 }
