d8a732039932e232c6cb3bf7ee04886c359c8288
shut_uper <ttatta3adpot@gmail.com>
2021-11-23 21:42:38 +0200

    ref/ history refactoring

--- src/app/components/home-page/history/history.component.ts
+++ src/app/components/home-page/history/history.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {ConfirmationService, LazyLoadEvent, MessageService} from "primeng/api";
+import {ConfirmationService, LazyLoadEvent} from "primeng/api";
 import {HistoryService} from "../../../service/history.service";
 import {History} from "../../../model/email";
-import {TableHeaderCheckbox} from "primeng/table";
+import {PopupMessageService} from "../../../service/utils/popup-message.service";
 
 @Component({
   selector: 'app-history',
@@ -11,52 +11,42 @@ import {TableHeaderCheckbox} from "primeng/table";
 })
 export class HistoryComponent implements OnInit {
 
-  numberOfRows: number = 15;
-
-  totalRecords!: number;
-
-  productDialog: boolean = false;
-
-  loading: boolean = false;
-
-  firstLoad: boolean = true;
-
-  checked: boolean = false;
-
-  first: number = 0;
-
-  emails!: History[];
-
-  email!: History;
+  numberOfRows: number = 15; // default number of table rows
+  totalRecords!: number;  // total number of emails in history
+  loading: boolean = false; // icon of loading before emails loaded
+  firstLoad: boolean = true; // marker for sorting by emails ids (for first load)
+  isChecked: boolean = false; // all emails selected
+  first: number = 0; // xz 3a4em yzhe ne nado naverno
+  emails!: History[]; // all emails on current page (lazy loaded)
+  selectedEmails: History[] = []; // emails selected by checkboxes
 
 
-  selectedEmails: History[] = [];
-
-  submitted: boolean = false;
-
-  constructor(private messageService: MessageService,
+  constructor(private popupMessageService: PopupMessageService,
               private confirmationService: ConfirmationService,
               private historyService: HistoryService) {
   }
 
-  unselect() {
-    this.selectedEmails = [];
-  }
-
-  select() {
-    if (this.checked) {
-      this.selectedEmails = this.emails;
-    } else {
-      this.selectedEmails = [];
-    }
-  }
-
   ngOnInit() {
     this.loading = true;
+
     this.historyService.getTotalNumberOfRows().subscribe(totalRecords => this.totalRecords = totalRecords);
+
     this.getCurrentHistoryPage(0, this.numberOfRows, 'id', -1);
   }
 
+  selectOrUnselectAllEmails() {
+    if (this.isChecked)
+      this.selectedEmails = this.emails;
+    else
+      this.selectedEmails = [];
+  }
+
+  sliceLongString(str: string){
+    if (str.length>=65)
+      return str.slice(0,64).concat('...');
+    else
+      return str;
+  }
 
   private getCurrentHistoryPage(page: number, rows: number, sortFiled: string, sortOrder: number) {
     this.historyService.getPaginatedHistory(page, rows, sortFiled, sortOrder).subscribe({
@@ -66,31 +56,30 @@ export class HistoryComponent implements OnInit {
       },
       error: () => {
         setTimeout(() => {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Failed',
-            detail: "Couldn't load emails history!"
-          });
+          this.popupMessageService.showFailed("Couldn't load emails history!");
           this.loading = false;
           this.getCurrentHistoryPage(0, this.numberOfRows, 'id', -1);
-        }, 5000)
+        }, 5000)  // if failed to load data, recursively try to load emails after 5 seconds
       }
     });
   }
 
-  loadEmails(event: LazyLoadEvent) {
+  loadEmails(event: LazyLoadEvent) {  //loading emails after changing page/sorter
     console.log(event);
-    this.loading = true;
-    if (event.rows !== undefined) {
-      let page = (event.first === undefined ? 0 : event.first) / event.rows;
-      let sortField = (event.sortField === undefined) ? 'id' : event.sortField;
-      let sortOrder = (event.sortOrder === undefined) ? -1 : event.sortOrder;
+    if (event.rows) {
+      this.loading = true;
+
+      let page = ((!event.first) ? 0 : event.first) / event.rows;
+      let sortField = (!event.sortField) ? 'id' : event.sortField;
+      let sortOrder = (!event.sortOrder) ? -1 : event.sortOrder;
       this.first = event.first || 0;
-      if (this.firstLoad || (sortField === 'id')) {
+
+      if (this.firstLoad || (sortField === 'id')) {  //default sorting by emails ids
         this.firstLoad = false;
         sortOrder = -1;
       }
-      window.scrollTo(0, 0);
+
+      window.scrollTo(0, 0); // jump to top of the page before loading new content
       this.getCurrentHistoryPage(page, event.rows, sortField, sortOrder);
     }
   }
@@ -102,78 +91,46 @@ export class HistoryComponent implements OnInit {
       header: 'Confirm',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-
-        let ids = selected.map(x => x.id);
+        let ids = selected.map(x => x.id);  // get array of selected emails' ids
         this.historyService.deleteEmailsByIds(ids).subscribe({
-          next: () => {
-            this.emails = this.emails.filter(val => !selected.includes(val));
-            this.getCurrentHistoryPage(0, this.numberOfRows, 'id', -1);
-            this.checked = false;
-            this.totalRecords = this.totalRecords - selected.length;
-            this.selectedEmails = [];
-            this.messageService.add({
-              severity: 'success',
-              summary: 'Successful',
-              detail: 'Products Deleted',
-              life: 3000
-            });
-          },
-          error: () => {
-            this.selectedEmails = [];
-            this.messageService.add({
-              severity: 'error',
-              summary: 'Failed',
-              detail: "Couldn't delete selected emails!"
-            });
+
+            next: () => {
+              this.emails = this.emails.filter(val => !selected.includes(val));
+              this.getCurrentHistoryPage(0, this.numberOfRows, 'id', -1);
+
+              this.isChecked = false;
+              this.totalRecords -= selected.length;
+              this.selectedEmails = [];
+              this.popupMessageService.showSuccess('Products are deleted!');
+            },
+            error: () => {
+              this.selectedEmails = [];
+              this.popupMessageService.showFailed("Couldn't delete selected emails!");
+            }
           }
-        })
+        )
       }
     });
   }
 
+
   deleteEmailById(id: number) {
     this.confirmationService.confirm({
-      message: 'Are you sure you want to delete selected email ?!',
+      message: 'Are you sure you want to delete selected email?!',
       header: 'Confirm',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
         this.historyService.deleteEmailById(id).subscribe({
           next: () => {
-            this.messageService.add({
-              severity: 'success',
-              summary: 'Successful',
-              detail: 'Email Deleted',
-              life: 3000
-            });
+            this.popupMessageService.showSuccess('Email is deleted!');
             this.emails = this.emails.filter(val => val.id !== id);
             this.totalRecords--;
           },
-          error: () => this.messageService.add({
-            severity: 'error',
-            summary: 'Failed',
-            detail: 'Email is not deleted',
-            life: 3000
-          })
+          error: () => this.popupMessageService.showFailed('Email is not deleted!')
         })
       }
     });
   }
 
-  hideDialog() {
-    this.productDialog = false;
-    this.submitted = false;
-  }
-
-// findIndexById(id: string): number {
-//   let index = -1;
-//   for (let i = 0; i < this.products.length; i++) {
-//     if (this.products[i].id === id) {
-//       index = i;
-//       break;
-//     }
-//   }
-//
-//   return index;
-// }
 
 }
---
+++ src/app/service/utils/popup-message.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {MessageService} from "primeng/api";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PopupMessageService {
+
+  constructor(private messageService: MessageService) { }
+
+  showSuccess(message: string){
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Successful',
+      detail: message
+    });
+  }
+
+  showFailed(message: string){
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Failed',
+      detail: message
+    });
+  }
+
+}
