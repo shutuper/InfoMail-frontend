fe733f56ce96c12ffceeecdf9970833d5fc4469a
Lastzlo <z.last.zlo@gmail.com>
2021-11-24 01:33:44 +0200

    feat inject PopupMessageService, add deleteTemplate, deleteSelectedTemplates

--- src/app/components/home-page/templates/templates.component.ts
+++ src/app/components/home-page/templates/templates.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {EmailTemplate} from "../../../model/email";
 import {TemplateService} from "../../../service/template.service";
+import {PopupMessageService} from "../../../service/utils/popup-message.service";
 
 @Component({
   selector: 'app-templates',
@@ -10,8 +11,10 @@ import {TemplateService} from "../../../service/template.service";
 export class TemplatesComponent implements OnInit {
 
   templates: EmailTemplate[] = [];
+  template!: EmailTemplate;
+  selectedTemplates: EmailTemplate[] = [];
 
-  constructor(private templateService: TemplateService) { }
+  constructor(private templateService: TemplateService, private popupMessageService: PopupMessageService) { }
 
   ngOnInit(): void {
     this.getTemplates();
@@ -22,15 +25,39 @@ export class TemplatesComponent implements OnInit {
   }
 
 
-  deleteProduct(template: EmailTemplate) {
-
+  deleteTemplate(template: EmailTemplate) {
+    console.log("deleteTemplate: ", template);
+    this.templateService.deleteEmailTemplateById(template.id).subscribe({
+      next: () => {
+        this.popupMessageService.showSuccess('Template is deleted!');
+        this.templates = this.templates.filter(val => val.id !== template.id);
+      },
+      error: () => this.popupMessageService.showFailed('Template is not deleted!')
+    })
   }
 
-  editProduct(template: EmailTemplate) {
-
+  editTemplate(template: EmailTemplate) {
+    console.log("editTemplate: ", template);
   }
 
   sliceText(text: string, maxLength: number): string {
     return (text.length >= maxLength) ? (text.slice(0, maxLength) + '...') : text;
+  }
+
+  deleteSelectedTemplates() {
+    console.log("deleteSelectedTemplates: ", this.selectedTemplates);
+    let selected = this.selectedTemplates;
+    let ids = selected.map(x => x.id);  // get array of selected emails' ids
+    this.templateService.deleteEmailTemplatesByIds(ids).subscribe({
+      next: () => {
+        this.templates = this.templates.filter(val => !selected.includes(val));
+        this.selectedTemplates = [];
+        this.popupMessageService.showSuccess('Templates are deleted!');
+      },
+      error: () => {
+        this.selectedTemplates = [];
+        this.popupMessageService.showFailed("Couldn't delete selected templates!");
+      }
+    });
   }
 }
