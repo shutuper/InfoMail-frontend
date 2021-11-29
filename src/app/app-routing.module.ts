f12d0a6d26ccf3b4d93a8ccec71409887da8366d
shut_uper <ttatta3adpot@gmail.com>
2021-11-29 21:08:26 +0200

    ref/ module imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -50,8 +50,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    declarations: [],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
